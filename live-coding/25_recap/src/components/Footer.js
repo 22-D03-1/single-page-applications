c03d13bb85df1adf791e8cdc4f68061ce880dcb5
Ferdi <hoskeee@gmail.com>
2022-11-16 17:01:57 +0100

    updated footer

--- live-coding/25_recap/src/components/Footer.js
+++ live-coding/25_recap/src/components/Footer.js
@@ -2,9 +2,9 @@ import {Link} from "react-router-dom"
 
 export default function Footer(){
     return (
-        <div>
-            <Link to="/impressum">Impressum</Link>
-            <Link to="/datenschutz">Datenschutz</Link>
+        <div className="d-flex justify-content-center my-4">
+            <Link className="mx-2" to="/impressum">Impressum</Link>
+            <Link className="mx-2" to="/datenschutz">Datenschutz</Link>
         </div>
     )
 }
