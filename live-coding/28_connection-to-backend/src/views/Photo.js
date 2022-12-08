73388d4edae76563fd2722ddb4192f56538a4584
Ferdi <hoskeee@gmail.com>
2022-12-08 13:16:07 +0100

    comments

--- live-coding/28_connection-to-backend/src/views/Photo.js
+++ live-coding/28_connection-to-backend/src/views/Photo.js
@@ -8,6 +8,23 @@ export default function(){
 
     const [photo, setPhoto] = useState({})
 
+    /**
+     * Um unsere React App (View) mit unserem Backend (Controller und "Model") zu verbinden,
+     * machen wir lediglich ein fetch an unseren localhost. Da fetch das Wort "localhost" mag,
+     * benutzen wir die dazugehörige IP Adresse. 
+     * 
+     * Frontend und Backend befinden sich im selben Hafen (localhost) und haben zwei 
+     * Stegnummern (3000 und 4000) über die sie erreichbar sind.
+     * 
+     * In einem real world example wäre das optimale Setup, dass sich unser FE und das BE in
+     * zwei Docker Container befinden und über festgelegte Wege miteinander kommunizieren.
+     * Leider sprengt Docker den Rahmen von diese, Kurs. Ich kann euch aber empfehlen, euch das
+     * selbstständig anzuschauen.
+     * 
+     * Den fetch machen wir in Abhängigkeit von unserem Parameter, heißt immer wenn sich der 
+     * Parameter ändert rufen wir neu die Daten ab
+     */
+
     useEffect(()=>{
         fetch(`http://172.20.10.2:4000/photos/${photoId}`)
             .then(response => response.json())
