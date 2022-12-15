4bca557670e18b8578c48c9055cdc7ef8cff1734
Ferdi <hoskeee@gmail.com>
2022-12-15 12:09:44 +0100

    bug fix filename and url

--- live-coding/28.1_connection-to-backend/src/compontents/PhotoPreview.js
+++ live-coding/28.1_connection-to-backend/src/compontents/PhotoPreview.js
@@ -2,10 +2,11 @@ import { Link } from "react-router-dom"
 
 export default function({photo}) {
     // /files/photos/:filename
+    const imageSrc = photo.filename ? `/files/photos/${photo.filename}` : photo.url
     return (
         <div className="img-preview">
             <Link to ={"/photos/" + photo.id}>
-                <img src={`/files/photos/${photo.filename}`} />
+                <img src={imageSrc} />
             </Link>
         </div>
     )
